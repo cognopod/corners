593e893016c59a30a048883cae90703297a5e566
Karl Bailey <karl@cognopod.com>
2019-03-31 16:26:09 -0400

    pass through color arguments to plotCorners

--- R/allCorners.R
+++ R/allCorners.R
@@ -1,13 +1,21 @@
 #' allCorners
-#' 
+#'
 #' allCorners allows you to plot all of the plots in a prepCorners() list
 #' @param cornerSet is an dataframe produced by prepNettle()
 #' @param outcomeLabel is a passthrough variable for the name of the outcome
+#' @param pathColor is the color of the on-path cases with the outcome
+#' @param othersColor is the color of the cases with the outcome not on the path
+#' @param pathNotColor is the color of the on-path cases without the outcome
+#' @param othersNotColor is the color of the irrelevant cases (without the outcome and not on the path)
 #' @export
 allCorners <- function(cornerSet, outcomeLabel = "OUTCOME") {
   PATHNOT <- sum(cornerSet$pimChart$NOT[which(cornerSet$pimChart$PATH == 1)])
   OTHNOT <- sum(cornerSet$pimChart$NOT[which(cornerSet$pimChart$PATH == 0)])
   PATHOUT <- sum(cornerSet$pimChart$OUT[which(cornerSet$pimChart$PATH == 1)])
   OTHOUT <- sum(cornerSet$pimChart$OUT[which(cornerSet$pimChart$PATH == 0)])
-  plotCorners(PATHOUT, OTHOUT, PATHNOT, OTHNOT, 0, "ALL PATHS", outcomeLabel)
+  plotCorners(PATHOUT, OTHOUT, PATHNOT, OTHNOT, 0, "ALL PATHS", outcomeLabel,
+              pathColor = pathColor,
+              othersColor = othersColor,
+              pathNotColor = pathNotColor,
+              othersNotColor = othersNotColor)
 }
--- R/fourCorners.R
+++ R/fourCorners.R
@@ -1,18 +1,26 @@
 #' fourCorners
-#' 
+#'
 #' fourCorners allows you to plot all of the plots in a prepCorners() list
 #' @param cornerSet is an dataframe produced by prepNettle()
 #' @param outcomeLabel is a passthrough variable for the name of the outcome
+#' @param pathColor is the color of the on-path cases with the outcome
+#' @param othersColor is the color of the cases with the outcome not on the path
+#' @param pathNotColor is the color of the on-path cases without the outcome
+#' @param othersNotColor is the color of the irrelevant cases (without the outcome and not on the path)
 #' @export
 fourCorners <- function(cornerSet, outcomeLabel = "OUTCOME") {
-  # draw a deadNettle plot for each path 
+  # draw a deadNettle plot for each path
   for (k in 1:nrow(cornerSet$pimCorners)) {
-    plotCorners(cornerSet$pimCorners$PATHOUT[k], 
-                    cornerSet$pimCorners$OTHOUT[k], 
-                    cornerSet$pimCorners$PATHNOT[k], 
-                    cornerSet$pimCorners$OTHNOT[k], 
-                    cornerSet$pimCorners$UNIQOUT[k], 
-                    row.names(cornerSet$pimCorners)[k], 
-                    outcomeLabel)
+    plotCorners(cornerSet$pimCorners$PATHOUT[k],
+                    cornerSet$pimCorners$OTHOUT[k],
+                    cornerSet$pimCorners$PATHNOT[k],
+                    cornerSet$pimCorners$OTHNOT[k],
+                    cornerSet$pimCorners$UNIQOUT[k],
+                    row.names(cornerSet$pimCorners)[k],
+                    outcomeLabel,
+                pathColor = pathColor,
+                othersColor = othersColor,
+                pathNotColor = pathNotColor,
+                othersNotColor = othersNotColor)
   }
 }
